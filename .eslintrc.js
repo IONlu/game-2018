a49cc0e9016bc3cfd32ed895140294d4d36d6065
Daniel Duton <daniel@duton.lu>
2018-10-19 15:28:11 +0200

    update configs

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,22 @@
 module.exports = {
-  root: true,
-  env: {
-    node: true
-  },
-  'extends': [
-    'plugin:vue/essential',
-    '@vue/standard'
-  ],
-  rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
-  },
-  parserOptions: {
-    parser: 'babel-eslint'
-  }
+    root: true,
+    env: {
+        node: true
+    },
+    'extends': [
+        'plugin:vue/recommended',
+        '@vue/standard'
+    ],
+    rules: {
+        'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+        'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+        // 4 spaces
+        'indent': ['error', 4, {
+            "SwitchCase": 1
+        }],
+        'vue/html-indent': ['error', 4]
+    },
+    parserOptions: {
+        parser: 'babel-eslint'
+    }
 }
