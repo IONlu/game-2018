91ea54dd00d4f50a63fef356418ef169606f3ae4
Daniel Duton <daniel@duton.lu>
2018-12-21 15:18:33 +0100

    fix howto

--- src/views/Howto.vue
+++ src/views/Howto.vue
@@ -1,40 +1,27 @@
 <template>
-    <div :class="$style.howto" />
+    <div :class="$style.container">
+        <img
+            :class="$style.howto"
+            src="../assets/svg/howto.svg"
+        >
+    </div>
 </template>
 
 <style module>
-
-    html,
-    body {
-        margin: 0;
-        padding: 0;
+    .container {
+        overflow-x: auto;
+        overflow-y: hidden;
         width: 100vw;
         height: 100vh;
-        overflow: hidden;
     }
 
     .howto {
-        overflow: auto;
-        color: white;
-        display: flex;
-        flex-direction: column;
-        width: 100vw;
-        height: 100vh;
-        font-family: Arial, sans-serif;
-        font-size: 4vmin;
-        background: url(../assets/svg/howto.svg) center center no-repeat fixed;
-        background-size: cover;
-
-        * {
-            box-sizing: border-box;
-        }
+        height: 100%;
     }
-
 </style>
 
 <script>
 export default {
     name: 'Howto'
 }
-
 </script>
