549ebff60ca5bac549f6ed5b7ee3d059e14c42cf
Christian Ries <ries_christian@hotmail.com>
2018-12-21 12:38:02 +0100

    add css cleanup

--- src/views/Highscores.vue
+++ src/views/Highscores.vue
@@ -52,6 +52,15 @@
 
 <style module>
 
+    html,
+    body {
+        margin: 0;
+        padding: 0;
+        width: 100vw;
+        height: 100vh;
+        overflow: hidden;
+    }
+
     .highscores {
         overflow: auto;
         color: white;
