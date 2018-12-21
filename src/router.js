f1e38ed754cc7fbd68ece1e10d9e6ba965ba363c
Christian Ries <ries_christian@hotmail.com>
2018-12-21 14:51:49 +0100

    add howto page

--- src/router.js
+++ src/router.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import MapSelector from './views/MapSelector.vue'
 import Highscores from './views/Highscores.vue'
+import Howto from './views/Howto.vue'
 import Game from './views/Game.vue'
 
 Vue.use(Router)
@@ -23,6 +24,11 @@ export default new Router({
             path: '/play/:mapName',
             name: 'Play',
             component: Game
+        },
+        {
+            path: '/howto',
+            name: 'Howto',
+            component: Howto
         }
     ]
 })
