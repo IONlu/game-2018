1e469ab01310290f4ed72d5dfcdc9340cb7fbd19
Daniel Duton <daniel@duton.lu>
2018-12-22 18:37:09 +0100

    code style

--- src/server/app.js
+++ src/server/app.js
@@ -1,9 +1,15 @@
-var express = require('express')
-var bodyParser = require('body-parser')
-var cors = require('cors')
-var app = express()
-var packageData = require('../../package.json')
-var path = require('path')
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const app = express()
+const packageData = require('../../package.json')
+const path = require('path')
+const hash = require('hash.js')
+
+const validateHash = (str, reqHash) => {
+    let UUID = reqHash.substr(-36)
+    return reqHash === hash.sha1().update(UUID + str).digest('hex') + '-' + UUID
+}
 
 app.use(express.static(path.resolve(__dirname, '..', '..', 'dist')))
 app.use(bodyParser())
@@ -13,12 +19,12 @@ const MongoClient = require('mongodb').MongoClient
 
 let db
 
-let allowedKeys = [
-    'player',
-    'waveReached',
-    'enemiesKilled',
-    'map'
-]
+let allowedKeys = {
+    player: 'string',
+    map: 'string',
+    waveReached: 'number',
+    enemiesKilled: 'number'
+}
 
 MongoClient.connect('mongodb://localhost:27017', (err, client) => {
     if (err) throw err
@@ -29,49 +35,46 @@ MongoClient.connect('mongodb://localhost:27017', (err, client) => {
     })
 })
 
-app.post('/highscores', (req, res) => {
-    if (req.body) {
-        let check = true
-        Object.keys(req.body).forEach((key) => {
-            if (!allowedKeys.includes(key)) {
-                check = false
-            } else {
-                allowedKeys.forEach((allowedKey) => {
-                    switch (allowedKey) {
-                        case 'player':
-                        case 'map':
-                            if (typeof req.body[allowedKey] !== 'string') {
-                                check = false
-                            }
-                            break
-                        case 'waveReached':
-                        case 'enemiesKilled':
-                            if (typeof req.body[allowedKey] !== 'number') {
-                                check = false
-                            }
-                            break
-                    }
-                })
-            }
-        })
-        if (check) {
-            let data = {
-                ...req.body,
-                version: packageData.version
-            }
-            db.collection('highscores').insertOne(data, (err, result) => {
-                if (err) throw err
-                db.collection('highscores').find({
-                    '_id': result.insertedId
-                }).toArray((err, results) => {
-                    if (err) throw err
-                    res.status(200).send(results)
-                })
-            })
-        } else {
-            res.status(400).send()
+const validateBody = body => {
+    for (let key in body) {
+        if (!allowedKeys.hasOwnProperty(key)) {
+            return false
         }
     }
+    for (let key in allowedKeys) {
+        if (!body.hasOwnProperty(key) || typeof body[key] !== allowedKeys[key]) {
+            return false
+        }
+    }
+}
+
+app.post('/highscores', (req, res) => {
+    return new Promise((resolve, reject) => {
+        let reqHash = (req.header('Authorization') || '').substr(7)
+        let reqBody = req.body
+        if (validateHash(reqHash)) {
+            let uuid = reqHash.substr(-36)
+            // is blacklisted
+            if (db.collection('blacklist').findOne({ uuid })) {
+                return reject(new Error('blacklisted'))
+            }
+
+            if (validateBody(reqBody)) {
+                let data = {
+                    ...reqBody,
+                    uuid,
+                    version: packageData.version
+                }
+                db.collection('highscores')
+                    .insertOne(data, err => err ? reject(err) : resolve())
+            } else {
+                db.collection('blacklist')
+                    .insertOne({ uuid }, () => resolve())
+            }
+        }
+    }).then(() => {
+        res.status(200).send(req.body)
+    })
 })
 
 app.get('/highscores', (req, res) => {
@@ -83,7 +86,6 @@ app.get('/highscores', (req, res) => {
         .limit(10)
         .toArray((err, results) => {
             if (err) throw err
-            // eslint-disable-next-line no-console
             res.send(results)
         })
 })
