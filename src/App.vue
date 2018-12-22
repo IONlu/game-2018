8b9a991d84de994b74bcbe509029eec0b2519051
Daniel Duton <daniel@duton.lu>
2018-12-22 08:18:50 +0100

    generate uuid

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,39 @@
 <template>
     <router-view/>
 </template>
+
+<script>
+import uuidv4 from 'uuid/v4'
+
+export default {
+    created () {
+        this.getUUID()
+        window.addEventListener('beforeunload', this.onBeforeUnload)
+    },
+
+    beforeDestroy() {
+        window.removeEventListener('beforeunload', this.onBeforeUnload)
+        this.saveUUID()
+    },
+
+    methods: {
+        getUUID () {
+            let UUID = localStorage.getItem('UUID')
+            if (!UUID) {
+                UUID = uuidv4()
+                localStorage.setItem('UUID', UUID)
+            }
+            this.$root.UUID = UUID
+            return UUID
+        },
+
+        saveUUID () {
+            localStorage.setItem('UUID', this.$root.UUID)
+        },
+
+        onBeforeUnload () {
+            this.saveUUID()
+        }
+    }
+}
+</script>
